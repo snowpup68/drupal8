0928985c19631bfceb4b4bf1fa67239afeca31d0
Jai Rajagopalan <jai@rajagopalan.com>
2016-02-05 11:56:56 -0600

    added module: backup and migrate

---
+++ sites/default/files/php/twig/bdd6b639_block--system.html.twig_3a1eb20f6cddc314c05576a59e531709b7fa9987aca3a0012f923aabc49aa962/bb7be3304c8a98a05fdb7258c57e6b35a84cc9b98831b2ec71a64f0d5671581a.php
@@ -0,0 +1,66 @@
+<?php
+
+/* themes/contrib/bootstrap/templates/block--system.html.twig */
+class __TwigTemplate_431b413cd6eda9701d1eac5ec4b49a78978bfb0120ee068295b6f2214a96c37d extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        // line 1
+        $this->parent = $this->loadTemplate("block--bare.html.twig", "themes/contrib/bootstrap/templates/block--system.html.twig", 1);
+        $this->blocks = array(
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "block--bare.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/contrib/bootstrap/templates/block--system.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  11 => 1,);
+    }
+}
+/* {% extends "block--bare.html.twig" %}*/
+/* */
